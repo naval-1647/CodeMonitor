880e826c3183b0e119fc325b959d4cb4e3b026eb
Naval jha <menavaljha@gmail.com>
2025-12-07 14:33:25 +0530

    Remove documentation and setup files from git tracking, add to .gitignore

--- start-backend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting CodeMentor AI Backend...
-echo.
-echo Backend will run on http://localhost:8000
-echo API Docs: http://localhost:8000/docs
-echo.
-cd backend
-python -m app
--- start-frontend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting CodeMentor AI Frontend...
-echo.
-echo Frontend will run on http://localhost:3000
-echo.
-cd frontend
-call npm run dev
